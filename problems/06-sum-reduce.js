8996921348816dd8a2c4c0b4625e933fd468be84
Christopher Cohen <ender9889@gmail.com>
2022-08-09 15:04:17 -0700

    prob6 complete

--- problems/06-sum-reduce.js
+++ problems/06-sum-reduce.js
@@ -12,14 +12,18 @@ console.log(sumWithReduce([])); // 0
 
 */
 
-let sumWithReduce = function(nums) {
-  // Your code here
+let sumWithReduce = function (nums) {
+    // Your code here
+    let sum = nums.reduce(function (accum, num) {
+        return accum += num;
+    }, 0)
+    return sum;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
-  module.exports = sumWithReduce;
+    module.exports = sumWithReduce;
 } catch (e) {
-  module.exports = null;
+    module.exports = null;
 }
