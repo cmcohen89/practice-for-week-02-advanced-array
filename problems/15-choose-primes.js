794eb3d71afb0f745f6b98884fcdea25f2454199
Christopher Cohen <ender9889@gmail.com>
2022-08-09 15:29:07 -0700

    prob15 complete

--- problems/15-choose-primes.js
+++ problems/15-choose-primes.js
@@ -18,12 +18,20 @@ console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
 
 */
 
-let isPrime = function(num) {
+let isPrime = function (num) {
     // Your code here
+    if (num < 2) return false;
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) return false
+    }
+    return true;
 }
 
-let choosePrimes = function(nums) {
+let choosePrimes = function (nums) {
     // Your code here
+    let primes = nums.filter(num => isPrime(num))
+    return primes;
 }
 
 // alternative solution using Array.filter
@@ -37,4 +45,4 @@ try {
     module.exports = choosePrimes;
 } catch (e) {
     module.exports = null;
-}
+}
